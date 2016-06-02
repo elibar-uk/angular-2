06b4723046c3c6233bff931450a7c81fcf12e5f5
Elia Bardashevich <ebardashevich@gmail.com>
2016-06-02 14:00:31 +0100

    adds adds and remove, tests passing

--- test/unit/ToDoController.spec.js
+++ test/unit/ToDoController.spec.js
@@ -6,11 +6,24 @@ describe('ToDoController', function() {
   beforeEach(inject(function($controller) {
     ctrl = $controller('ToDoController');
   }));
+
   it('initialises with a toDo', function() {
     var todos = [
       {text: "ToDo1", completed: true},
       {text: "ToDo2", completed: false}
     ];
     expect(ctrl.todos).toEqual(todos);
+  });
+
+  it('adds a new todo', function() {
+    ctrl.addToDo('NewTodo');
+    var todo = {text: "NewTodo", completed: false};
+    expect(ctrl.todos.pop()).toEqual(todo);
+  });
+
+  it('removes the last todo', function() {
+    var initialCount = ctrl.todos.length;
+    ctrl.removeToDo();
+    expect(ctrl.todos.length).toEqual(initialCount - 1);
   });
 });
